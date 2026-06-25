f7651d5c11149dc55407d8a2cef576e867e4829e
osoclos <osoclosus@gmail.com>
2026-06-25 10:41:53 +0800

    fixed incorrect version

--- Plugin.cs
+++ Plugin.cs
@@ -22,7 +22,7 @@ namespace CotlSaveExtractorLoader
         public const string Id = "mod.osoclos.cotl.save-extractor-loader";
         public const string Name = "Cult of the Lamb Save File Extractor and Loader";
 
-        public const string Version = "1.0.1.0";
+        public const string Version = "1.0.2.0";
     }
 
     [BepInPlugin(PluginMetadata.Id, PluginMetadata.Name, PluginMetadata.Version)]
